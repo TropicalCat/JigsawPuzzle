433423760428f073b47ac21e93646cba75bf02a0
TropicalCat <xuxu.beibei@hotmail.com>
2017-09-11 08:36:36 +0800

    init Game

---
+++ Assets/GE/Module/Framework/Module.cs
@@ -0,0 +1,13 @@
+﻿namespace GE
+{
+    public abstract class Module
+    {
+        /// <summary>
+        /// 调用它以释放模块
+        /// </summary>
+        public virtual void Release()
+        {
+			this.Log( typeof(Module).Name + " Release" );
+        }
+    }
+}
---
+++ Assets/GameFrameWork/GameMain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GE;
+
+public class GameMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debuger.EnableLog = true;
+		ModuleManager.Instance.CreateModule ("login");
+		ModuleManager.Instance.ShowModule ("login");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
