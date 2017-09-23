ea461d53c5990c9cb621b9b50de424a2a60d3d5b
徐旭 <xubeibei1@xiaomi.com>
2017-09-24 07:45:07 +0800

    拖动

---
+++ Assets/GameFrameWork/UI/Drag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour {
+
+	public  Canvas canvas;
+	RectTransform rectTransform;
+	// Use this for initialization
+	void Start () 
+	{ 
+		rectTransform = transform as RectTransform;
+		canvas = GameObject.Find("UIRoot").GetComponent<Canvas>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos;
+		if(RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, Input.mousePosition, canvas.worldCamera, out pos)){
+			rectTransform.anchoredPosition = pos;
+		}
+		
+	}
+}
