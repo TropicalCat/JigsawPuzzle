fb653c3cb9d65c68f3a50cd773d4dd838cb4eeaa
徐旭 <xubeibei1@xiaomi.com>
2017-09-27 20:59:48 +0800

    拖动

---
+++ Assets/GameFrameWork/Game/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GE;
+
+namespace GFW
+{
+	public class GameManager : ServiceModule<GameManager>
+	{
+		public GameManager()
+		{
+		}
+
+		public void Init()
+		{
+		}
+
+		public void Destory()
+		{
+		}
+
+		public void Clear()
+		{
+		}
+
+
+
+
+
+
+
+
+	}
+}
---
+++ Assets/GameFrameWork/Game/PieceManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GE;
+
+namespace GFW
+{
+	public class PieceManager : ServiceModule<PieceManager>
+	{
+
+		private int CurLevel;		//当前关卡	
+		private int CurDifficulty;	//当前难度
+
+
+
+		public PieceManager()
+		{
+		}
+
+		public void Init()
+		{
+			CurLevel=0;		//当前关卡	
+			CurDifficulty=0;
+		}
+
+		public void Destory()
+		{
+		}
+
+		public void Clear()
+		{
+		}
+
+
+
+
+
+	}
+}
+
+
