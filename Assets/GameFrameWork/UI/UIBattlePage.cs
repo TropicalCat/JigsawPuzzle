d1b7a20ed71bb985d23525dafef6d047ece5b4bc
TropicalCat <xuxu.beibei@hotmail.com>
2017-10-12 13:45:35 +0800

    隐藏显示底图

--- Assets/GameFrameWork/UI/UIBattlePage.cs
+++ Assets/GameFrameWork/UI/UIBattlePage.cs
@@ -9,14 +9,29 @@ namespace GFW
 {
 	public class UIBattlePage : UIPage 
 	{
+		[SerializeField]
+		private Button m_btnArtist;
+
+
 		protected override void OnOpen(object arg = null)
 		{
 			base.OnOpen (arg);
 			initPiece ();
+
+			if (m_btnArtist != null)
+			{
+				m_btnArtist.onClick.AddListener(OnArtist);
+			}
+
 		}
 
 		protected override void OnClose(object arg = null)
 		{
+			if (m_btnArtist != null)
+			{
+				m_btnArtist.onClick.RemoveAllListeners();
+			}
+
 			base.OnClose (arg);
 			//销毁掉窗口
 			Destroy (gameObject);
@@ -25,6 +40,8 @@ namespace GFW
 		void initPiece()
 		{
 			string pth = PieceManager.Instance.GetCurIllustration ();
+
+			Sprite pieceImage = Resources.Load(pth, typeof(Sprite)) as Sprite;
 
 			Image image = gameObject.transform.Find ("LeftDown").Find ("Piece").GetComponent<Image> ();
 			image.sprite = Resources.Load(pth, typeof(Sprite)) as Sprite;
@@ -37,6 +54,20 @@ namespace GFW
 
 			image = gameObject.transform.Find ("RightUp").Find ("Piece").GetComponent<Image> ();
 			image.sprite = Resources.Load(pth, typeof(Sprite)) as Sprite;
+
+			image = gameObject.transform.Find ("PieceZone").GetComponent<Image> ();
+			image.sprite = pieceImage;
+
+		}
+
+	
+		private void OnArtist()
+		{
+			GameObject obj = gameObject.transform.Find ("PieceZone").gameObject;
+			obj.SetActive (!obj.activeSelf);
+
+
+			
 		}
 	}
 }
